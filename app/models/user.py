be5f210f1eccf3f5647501ac54c1eac5e525af23
shablin <maksimchick@mail.ru>
2025-03-21 19:03:44 +0300

    `user` model for table `users`

---
+++ app/models/user.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from app.database import Base
+from datetime import datetime, timezone
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    age = Column(Integer, nullabe=False)
+    hashed_password = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.now(timezone.utc))
