2b38800bb247b0972605fd4c95e1324bf05501e2
shablin <maksimchick@mail.ru>
2025-03-21 18:42:51 +0300

    added and tested database config

--- app/config.py
+++ app/config.py
@@ -1,15 +1,30 @@
 from functools import lru_cache
 
-from pydantic import BaseModel
+from pydantic import BaseModel, computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Application(BaseModel):
     name: str = "Untitled"
 
+class Database(BaseModel):
+    scheme: str = "postgresql://"
+    user: str = "user"
+    password: str = "password"
+    db_name: str = "db_otvetnet"
+    port: int = 5432
+    docker_name: str = "db"
+
+    @computed_field
+    def url(self) -> str:
+        return (
+            f"{self.scheme}{self.user}:{self.password}@"
+            f"{self.docker_name}:{self.port}/{self.db_name}"
+        )
 
 class Settings(BaseSettings):
     app: Application = Application()
+    database: Database = Database()
     model_config = SettingsConfigDict(env_file=".env",
                                       env_nested_delimiter="__",
                                       env_file_encoding="utf-8")
